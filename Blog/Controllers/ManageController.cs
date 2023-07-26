932704d2d196affadd2ef64f544c8dcd93772f6f
Micah Winters <45744442+Micahaha@users.noreply.github.com>
2023-07-25 23:51:25 -0400

    added views to control and manage roles

--- Blog/Controllers/ManageController.cs
+++ Blog/Controllers/ManageController.cs
@@ -4,21 +4,25 @@ using BlogProject.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace BlogProject.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize]
     public class ManageController : Controller
     {
 
         private readonly BlogService blogService;
         private readonly ApplicationDbContext context;
         private readonly RoleManager<IdentityRole> roleManager;
-        public ManageController(BlogService blogService, ApplicationDbContext context, RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<ApplicationUser> userManager;
+        public ManageController(BlogService blogService, ApplicationDbContext context, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             this.blogService = blogService;
             this.context = context;
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
 
@@ -62,6 +66,104 @@ namespace BlogProject.Controllers
             }
 
             return View(role);
+        }
+
+        public async Task<IActionResult> Edit(string id) 
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            // Retrieve all the Users
+            foreach (var user in userManager.Users)
+            {
+                // If the user is in this role, add the username to
+                // Users property of EditRoleViewModel. This model
+                // object is then passed to the view for display
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+                return View(nameof(Error));
+            }
+            else
+            {
+                role.Name = model.RoleName;
+
+                // Update the Role using UpdateAsync
+                var result = await roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+        }
+
+        public IActionResult List()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
+
+        public IActionResult Add() 
+        {
+            var users = userManager.Users;
+            return View(users);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Add(string id) 
+        {
+
+            var user =  await userManager.FindByIdAsync(id);
+            var result = await userManager.AddToRoleAsync(user, "Admin");
+
+            if (result.Succeeded)
+                return RedirectToAction("Index", "home");
+
+            TempData["ToastMessage"] = "An error occurred: " + result.Errors;
+
+            var users = userManager.Users;
+            
+            return View(users);
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
 
---
+++ Blog/Models/EditRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogProject.Models
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<String>();
+        }
+
+        public string Id { get; set; }
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; }
+
+    }
+}
